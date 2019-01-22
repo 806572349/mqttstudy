eb34dd2cf2a4f6f544ec0124999df51516207810
EDZ <806572349@qq.com>
2019-01-22 10:47:04 +0800

    json处理

--- src/main/java/com/nemo/mqtt/study/DefaultMessageHandler.java
+++ src/main/java/com/nemo/mqtt/study/DefaultMessageHandler.java
@@ -1,12 +1,14 @@
 package com.nemo.mqtt.study;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.handler.AbstractMessageHandler;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.MessagingException;
+import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
@@ -22,7 +24,10 @@ public class DefaultMessageHandler extends AbstractMessageHandler {
 
         String payload = message.getPayload().toString();
         JsonData jsonData = JSON.parseObject(payload, JsonData.class);
+        Object data = jsonData.getData();
+        List<TransData> transData = JSONObject.parseArray(JSON.toJSONString(data), TransData.class);
 
+        System.out.println(transData);
         System.out.println(payload);
         MessageHeaders headers = message.getHeaders();
 //        headers.entrySet().forEach(a-> System.out.println("key:"+a.getKey()+"value:"+a.getValue()));
